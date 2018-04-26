4cb3b8570e6b54215e5b4b9cf74f4f1f4611b8a2
Ian Carpenter <ianhcarpenter@gmail.com>
2018-04-26 22:16:25 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace MongoInserter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var connectionString = "mongodb://localhost:27017";
+
+            var client = new MongoClient(connectionString);
+
+            IMongoDatabase db = client.GetDatabase("DotNetCore");
+
+            IMongoCollection<BsonDocument> collection = db.GetCollection<BsonDocument>("docs");
+
+            var document = new BsonDocument
+            {
+                {"theKey", "theValue" }
+            };
+
+            collection.InsertOne(document);
+        }
+    }
+}
